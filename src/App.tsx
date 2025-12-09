4c3975aa9a77d54b8da58471bad27cf9c5f90d18
bigxeung <202011633@jbnu.ac.kr>
2025-12-09 22:35:29 +0900

    fix: remove react-transition-group for React 19 compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import ProtectedRoute from './components/ProtectedRoute';
 import { isAuthenticated } from './utils/auth';
@@ -11,80 +10,11 @@ import Popular from './pages/Popular';
 import Search from './pages/Search';
 import Wishlist from './pages/Wishlist';
 
-function AnimatedRoutes() {
-  const location = useLocation();
-
-  return (
-    <TransitionGroup>
-      <CSSTransition
-        key={location.pathname}
-        timeout={300}
-        classNames="page"
-        unmountOnExit
-      >
-        <div className="page">
-          <Routes location={location}>
-            {/* 공개 라우트 - 로그인 페이지 */}
-            <Route
-              path="/signin"
-              element={
-                isAuthenticated() ? (
-                  <Navigate to="/" replace />
-                ) : (
-                  <SignIn />
-                )
-              }
-            />
-
-            {/* 보호된 라우트들 */}
-            <Route
-              path="/"
-              element={
-                <ProtectedRoute>
-                  <Home />
-                </ProtectedRoute>
-              }
-            />
-
-            <Route
-              path="/popular"
-              element={
-                <ProtectedRoute>
-                  <Popular />
-                </ProtectedRoute>
-              }
-            />
-
-            <Route
-              path="/search"
-              element={
-                <ProtectedRoute>
-                  <Search />
-                </ProtectedRoute>
-              }
-            />
-
-            <Route
-              path="/wishlist"
-              element={
-                <ProtectedRoute>
-                  <Wishlist />
-                </ProtectedRoute>
-              }
-            />
-
-            {/* 404 처리 */}
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </div>
-      </CSSTransition>
-    </TransitionGroup>
-  );
-}
+const basename = import.meta.env.BASE_URL;
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <Toaster
         position="top-center"
         toastOptions={{
@@ -95,7 +25,61 @@ function App() {
           },
         }}
       />
-      <AnimatedRoutes />
+      <div className="page-wrapper">
+        <Routes>
+          {/* 공개 라우트 - 로그인 페이지 */}
+          <Route
+            path="/signin"
+            element={
+              isAuthenticated() ? (
+                <Navigate to="/" replace />
+              ) : (
+                <SignIn />
+              )
+            }
+          />
+
+          {/* 보호된 라우트들 */}
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/popular"
+            element={
+              <ProtectedRoute>
+                <Popular />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/search"
+            element={
+              <ProtectedRoute>
+                <Search />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/wishlist"
+            element={
+              <ProtectedRoute>
+                <Wishlist />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* 404 처리 */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
